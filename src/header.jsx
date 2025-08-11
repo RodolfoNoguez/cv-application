82db9dce9289e38c3ad129262e6916d2abd0750a
Rodolfo Noguez <rodolfonoguez10@gmail.com>
2025-08-11 18:42:47 -0500

    header done

---
+++ src/header.jsx
@@ -0,0 +1,14 @@
+
+export default function Header(){
+    return(
+        <header className="cv-header">
+            <h1 className="name">Rodolfo Noguez</h1>
+            <section>
+                <p><a href="mailto:rodolfonoguez10@gmail.com">rodolfonoguez10@gmail.com</a></p>
+                <p><a target="_blank" rel="noopener noreferrer" href="https://github.com/RodolfoNoguez">https://github.com/RodolfoNoguez</a></p>
+                <p><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/rodolfonoguez/">https://www.linkedin.com/in/rodolfonoguez/</a></p>
+                <p><a href="tel:9404429524">(940)442-9524</a></p>
+            </section>
+        </header>
+    )
+}
